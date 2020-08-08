4b61dcb9689441b750d4dca17fe846826df2c1b5
Dane M Ogden <DaneOgden1@gmail.com>
2020-08-08 13:13:02 -0500

    no meaningful change

--- js-playground/REST-API-crash/gitHubAPI.js
+++ js-playground/REST-API-crash/gitHubAPI.js
@@ -1,8 +1,8 @@
 const btnRepos = document.getElementById('btnRepos');
 btnRepos.addEventListener('click', getRepos);
 async function getRepos() {
-	const url = 'https://api.github.com/users/dane-git/repos';
 	const response = await fetch(url);
+	const url = 'https://api.github.com/users/dane-git/repos';
 	const result = await response.json();
 	console.log(result);
 }
